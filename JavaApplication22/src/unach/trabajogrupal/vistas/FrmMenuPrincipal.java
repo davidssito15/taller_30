bd442062ab9b09495b049198260df2128bace914
Usuario <Usuario@172.30.3.201>
2018-01-30 17:55:00 -0500

    david campos MENUPRINCIPLA

--- JavaApplication22/src/unach/trabajogrupal/vistas/FrmMenuPrincipal.java
+++ JavaApplication22/src/unach/trabajogrupal/vistas/FrmMenuPrincipal.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package unach.trabajogrupal.vistas;
 
 import java.awt.BorderLayout;
@@ -13,32 +9,48 @@ import javax.swing.*;
 
 public class FrmMenuPrincipal extends JFrame {
 
-    JMenuBar mnbPrincipal;
+   JMenuBar mnbPrincipal;
     JMenu mnInicio;
     JMenuItem mniLogin;
     JMenuItem mniSalir;
-
-    JMenu mnTerminal;
-    JMenuItem mniNuevoTerminal;
-    JMenuItem mniModificaTerminal;
-    JMenuItem mniListaTerminal;
+    
     JMenu mnAutobus;
     JMenuItem mniNuevoAutobus;
     JMenuItem mniModificaAutobus;
+    JMenuItem mniEliminaAutobus;
+    JMenuItem mniBuscaAutobus;
     JMenuItem mniListaAutobus;
+        
+
+        
     JMenu mnRegistro;
     JMenuItem mniNuevoRegistro;
     JMenuItem mniModificaRegistro;
+    JMenuItem mniEliminaRegistro;
+    JMenuItem mniBuscaRegistro;
     JMenuItem mniListaRegistro;
-
+    
+    
+    JMenu mnTerminal;
+    JMenuItem mniNuevoTerminal;
+    JMenuItem mniModificaTerminal;
+    JMenuItem mniEliminaTerminal;
+    JMenuItem mniBuscaTerminal;
+    JMenuItem mniListaTerminal;
+    
+    
+    
     JDesktopPane dkpEscritorio;
-
     public FrmMenuPrincipal() {
-        dkpEscritorio = new JDesktopPane();
+        dkpEscritorio= new JDesktopPane();
         dkpEscritorio.setBackground(new Color(240, 240, 240));
+        
+        setTitle("***** BASE DE DATOS TRASPORTE****");
+        dkpEscritorio.setBackground(Color.GRAY);
+        
         mnbPrincipal = new JMenuBar();
         mnInicio = new JMenu("Inicio");
-        mniLogin = new JMenuItem("Iniciar Secion");
+        mniLogin = new JMenuItem("Iniciar Sesión");
         mniSalir = new JMenuItem("Salir");
         mniSalir.addActionListener(new ActionListener() {
             @Override
@@ -46,115 +58,134 @@ public class FrmMenuPrincipal extends JFrame {
                 mniSalirActionPerformed(e);
             }
         });
-        mnTerminal = new JMenu("TERMINAL");
-        mnTerminal = new JMenu("Terminal");
-        mnAutobus = new JMenu("Autobus");
-        mnRegistro = new JMenu("Registro");
-        mniNuevoTerminal = new JMenuItem("Nuevo");
+        mnAutobus= new JMenu("Autobus");
+        mniNuevoAutobus= new JMenuItem("Nuevo");
+        mniNuevoAutobus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mniNuevoAutobusActionPerformed(e);
+            }
+        });
+        mniModificaAutobus= new JMenuItem("Modifica");
+        mniEliminaAutobus= new JMenuItem("Elimina");
+        mniBuscaAutobus= new JMenuItem("Busca");
+        mniListaAutobus= new JMenuItem("Lista");
+        mniListaAutobus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               // mniListeAutobusActionPerformed(e);
+            }
+        });
+        
+        
+        mnAutobus.add(mniNuevoAutobus);
+        mnAutobus.add(mniModificaAutobus);
+        mnAutobus.add(mniEliminaAutobus);
+        mnAutobus.addSeparator();
+        mnAutobus.add(mniBuscaAutobus);
+        mnAutobus.add(mniListaAutobus);
+        
+        
+         mnRegistro= new JMenu("Registro");
+        mniNuevoRegistro= new JMenuItem("Nuevo");
+        mniNuevoRegistro.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            //    mniNuevoRegistroActionPerformed(e);
+            }
+        });
+        mniModificaRegistro= new JMenuItem("Modifica");
+        mniEliminaRegistro= new JMenuItem("Elimina");
+        mniBuscaRegistro= new JMenuItem("Busca");
+        mniListaRegistro= new JMenuItem("Lista");
+        mniListaRegistro.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             //   mniListaRegistroActionPerformed(e);
+            }
+        });
+        
+        
+        mnRegistro.add(mniNuevoRegistro);
+        mnRegistro.add(mniModificaRegistro);
+        mnRegistro.add(mniEliminaRegistro);
+        mnRegistro.addSeparator();
+        mnRegistro.add(mniBuscaRegistro);
+        mnRegistro.add(mniListaRegistro);
+        
+        
+        
+        mnTerminal=new JMenu("Terminal");
+        mniNuevoTerminal=new JMenuItem("Nuevo");
         mniNuevoTerminal.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 mniNuevoTerminalActionPerformed(e);
-            }
+                }
         });
-        mniModificaTerminal = new JMenuItem("Modifica");
-        mniListaTerminal = new JMenuItem("Lista");
-
-        mniListaTerminal.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-              //  mniListaTerminalPerdormed(e);
-            }
-        });
-
-        mniNuevoAutobus = new JMenuItem("Nuevo");
-        mniNuevoAutobus.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            //    mniNuevoAutobus ActionPerformed(e
-            
-            }
-        });
-        mniNuevoAutobus = new JMenuItem("Modifica");
-        mniNuevoAutobus = new JMenuItem("Lista");
-
-        mniNuevoRegistro = new JMenuItem("Nuevo");
-        mniNuevoRegistro.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-              //  mniNuevoRegistroActionPerformed(e);
-            }
-        });
-        mniModificaRegistro = new JMenuItem("Modifica");
-
-        mniListaRegistro = new JMenuItem("Lista");
-
-        mniListaRegistro.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               // mniListaRegistroActionPerdormed(e);
-            }
-        });
-
+        
+        mniModificaTerminal=new JMenuItem("Modifica");
+        mniEliminaTerminal=new JMenuItem("Elimina");
+        mniBuscaTerminal=new JMenu("Buscar");
+        mniListaTerminal=new JMenu("Lista");
+        
         mnTerminal.add(mniNuevoTerminal);
         mnTerminal.add(mniModificaTerminal);
+        mnTerminal.add(mniEliminaTerminal);
         mnTerminal.addSeparator();
-
+        mnTerminal.add(mniBuscaTerminal);
         mnTerminal.add(mniListaTerminal);
+        
+                
+        
         mnbPrincipal.add(mnInicio);
-        mnbPrincipal.add(mnTerminal);
         mnbPrincipal.add(mnAutobus);
         mnbPrincipal.add(mnRegistro);
-
+        mnbPrincipal.add(mnTerminal);
+     
         mnInicio.add(mniLogin);
         mnInicio.add(mniSalir);
-
-        mnAutobus.add(mniNuevoAutobus);
-        mnAutobus.add(mniModificaAutobus);
-
-        mnAutobus.addSeparator();
-
-        mnAutobus.add(mniListaAutobus);
-
-        mnRegistro.add(mniNuevoRegistro);
-        mnRegistro.add(mniModificaRegistro);
-
-        mnRegistro.addSeparator();
-
-        mnRegistro.add(mniListaRegistro);
-
+        
         this.setLayout(new BorderLayout());
         this.add(mnbPrincipal, BorderLayout.NORTH);
         this.add(dkpEscritorio, BorderLayout.CENTER);
-        this.setExtendedState(MAXIMIZED_BOTH);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setExtendedState(MAXIMIZED_BOTH); //Para maximizar
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); //Para terminar el programa
     }
-
-    public void mniSalirActionPerformed(ActionEvent e) {
-        System.exit(0);
-    }
-
-    public void mniNuevoTerminalActionPerformed(ActionEvent e) {
-        FrmNuevoTerminal frm = new FrmNuevoTerminal();
-        dkpEscritorio.add(frm);
-        frm.setVisible(true);
-    }
-
-//    public void mniNuevoAutobusActionPerformed(ActionEvent e) {
-//        FrmNuevoAutobus frm = new FrmNuevoAutobus();
-//        dkpEscritorio.add(frm);
-//        frm.setVisible(true);
-//    }
-
-//    public void mniNuevoRegistrotrActionPerformed(ActionEvent e) {
+    
+//    public void mniListaDocenteActionPerformed(ActionEvent e){
+//        FrmListaDocente frmD=new FrmListaDocente();
+//        dkpEscritorio.add(frmD);
+//        frmD.setVisible(true);
+//     } 
+//    public void mniListaEstudianteActionPerformed(ActionEvent e){
+//        FrmListaEstudiante frmLE=new FrmListaEstudiante();
+//        dkpEscritorio.add(frmLE);
+//        frmLE.setVisible(true);
+//     }
+      
+    
+    public void mniNuevoTerminalActionPerformed(ActionEvent e){
+        FrmNuevoTerminal frmM=new FrmNuevoTerminal();
+        dkpEscritorio.add(frmM);
+        frmM.setVisible(true);
+     }    
+//     public void mniNuevoRegistroActionPerformed(ActionEvent e){
 //        FrmNuevoRegistro frm = new FrmNuevoRegistro();
 //        dkpEscritorio.add(frm);
 //        frm.setVisible(true);
 //    }
+    public void mniNuevoAutobusActionPerformed(ActionEvent e){
+        FrmNuevoAutobus frm = new FrmNuevoAutobus();
+        dkpEscritorio.add(frm);
+        frm.setVisible(true);
+    }
+    public void mniSalirActionPerformed(ActionEvent e){
+        System.exit(0);
+    }
     
     public static void main(String[] args) {
-        FrmMenuPrincipal frmMEnu = new FrmMenuPrincipal();
-        frmMEnu.setVisible(true);
-
+        FrmMenuPrincipal frm = new FrmMenuPrincipal();
+        frm.setVisible(true);
     }
 }
